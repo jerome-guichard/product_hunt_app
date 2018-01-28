e9e90234fe1a8076057ef120f8258e5b6b7d2230
jerome-guichard <guichard_jerome@yahoo.fr>
2018-01-28 16:25:57 +0100

    Change service enpoint to use pagination as recommended on api doc

--- ProductHuntApp/app/core/post/post.service.js
+++ ProductHuntApp/app/core/post/post.service.js
@@ -12,28 +12,21 @@ angular.
         
         var service = {
             // DATA
-            getList: getList,
-            getNewerPostList: getNewerPostList,
-            getOlderPostList:getOlderPostList
-            
+            getList: getList     
         };
 
         return service;
         
         // Get all post list
-        function getList(additionalParam){
+        function getList(pageId){
 
             var deferred = $q.defer();
-            // if additionalParam is not defined, define it as ''
-            if(typeof additionalParam === 'undefined'){additionalParam='';}
             
             // http request
             $http({
                 //Endpoint: posts?all
-                //Request string: sort posts by creation date in a descending order 
-                //Request string: limit the number of results
-                //Additional Request string: get post older/older than a given post 
-                url: url+'posts/all?sort_by=created_at&order=desc&per_page='+postPerPage+additionalParam,
+                //Request string: page nnumber and per_page number of post
+                url: url+'posts/all?page='+pageId.toString()+'&per_page='+postPerPage,
                 method: 'GET',
                 }).success(function(data,status,headers,config){
                     deferred.resolve(data);
@@ -42,16 +35,6 @@ angular.
                 });
 
             return deferred.promise;
-        }
-        
-        // Get newer posts than the post represented by its id
-        function getNewerPostList(id){
-            return getList('&newer='+id.toString());
-        }
-        
-        // // Get older posts than the post represented by its id
-        function getOlderPostList(id){
-            return getList('&older='+id.toString());
         }
     }
  
