c5cc3ca5874186be030e162a65ad482313b542fa
Max <Max@Max-HP>
2018-01-28 13:06:54 +0100

    Add functions to get older/newer posts

--- ProductHuntApp/app/post-list/post-list.component.js
+++ ProductHuntApp/app/post-list/post-list.component.js
@@ -7,27 +7,49 @@ angular.
     templateUrl: 'post-list/post-list.template.html',
     controller: ['PostService','$scope','$log',
       function PostListController(PostService,$scope,$log) {
-        
-        
-        $scope.posts =[];       
-        $scope.daysAgo = "0";        
-        $scope.cat = "tech";
-        
-        // Get today's topic on page load
-        getPostList($scope.daysAgo);
+          
+        // Init posts structure
+        $scope.posts = [];
+        // Get latest posts on page load
+        getPostList();
        
        // Function triggered on "days ago" select change 
-       $scope.update = function() {
-           console.log("Changes detected");
-           getPostList($scope.daysAgo);
+       $scope.getNewerPostList = function() {
+           // Retrieve newer posts (newer than the current older post) 
+           PostService.getNewerPostList($scope.posts.currentNewerPostId ).then(function(data) {
+               // Bind new list of post 
+               $scope.posts.list =data.posts;
+                // Update scope values 
+                $scope.posts.currentNewerPostId = $scope.posts.list[0].id;
+                $scope.posts.currentOlderPostId = $scope.posts.list[$scope.posts.list.length-1].id;
+            });
         }
-        function getPostList(daysAgo){
-            
-            console.log("Changes detected");
-            console.log(daysAgo);          
-            PostService.getList(daysAgo).then(function(data) {
+        
+        $scope.getOlderPostList = function() {
+           // Retrieve older posts (older than the current older post) 
+           PostService.getOlderPostList($scope.posts.currentOlderPostId).then(function(data) {
+                // Bind new list of post
+                $scope.posts.list =data.posts;
+                // Update scope values 
+                $scope.posts.currentNewerPostId = $scope.posts.list[0].id;
+                $scope.posts.currentOlderPostId = $scope.posts.list[$scope.posts.list.length-1].id;
+                
+            });
+        }
+        
+        function getPostList(){                   
+            PostService.getList().then(function(data) {
+                console.log('Get latest posts');
                 console.log(data);
-                $scope.posts =data.posts;
+                
+                // Bind list of posts
+                $scope.posts.list =data.posts;
+                // Store latest post id to disable 'next button' when the current newer post id is the latest
+                $scope.posts.latestPostId = $scope.posts.list[0].id;
+                // Update scope values 
+                $scope.posts.currentNewerPostId = $scope.posts.list[0].id;
+                $scope.posts.currentOlderPostId = $scope.posts.list[$scope.posts.list.length-1].id;;
+
             });
         }
       }
