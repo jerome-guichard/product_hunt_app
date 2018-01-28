58a0ed23a2cfd33507fb9b5bcd64c57e04931214
Max <Max@Max-HP>
2018-01-28 13:05:46 +0100

    Get posts ordered by creation date in descending order Handle older/newer than a specific post request

--- ProductHuntApp/app/core/post/post.service.js
+++ ProductHuntApp/app/core/post/post.service.js
@@ -8,37 +8,50 @@ angular.
     function PostService(appEnv,$http,$q) {
         
         var url = appEnv.url;
+        var postPerPage = appEnv.postPerPage;
         
         var service = {
             // DATA
-            getList: getList
+            getList: getList,
+            getNewerPostList: getNewerPostList,
+            getOlderPostList:getOlderPostList
             
         };
 
         return service;
         
         // Get all post list
-        function getList(daysAgo){
+        function getList(additionalParam){
 
             var deferred = $q.defer();
-
+            // if additionalParam is not defined, define it as ''
+            if(typeof additionalParam === 'undefined'){additionalParam='';}
+            
+            // http request
             $http({
-
-                url: url+'posts?days_ago='+daysAgo,
+                //Endpoint: posts?all
+                //Request string: sort posts by creation date in a descending order 
+                //Request string: limit the number of results
+                //Additional Request string: get post older/older than a given post 
+                url: url+'posts/all?sort_by=created_at&order=desc&per_page='+postPerPage+additionalParam,
                 method: 'GET',
-
                 }).success(function(data,status,headers,config){
-                    //$log.debug("Get Segment List");
                     deferred.resolve(data);
-
                 }).error(function(){
-                    //$log.debug('Error trying to get segment List');
-                    //data = "null";
                     deferred.reject("null");
                 });
 
             return deferred.promise;
         }
         
+        // Get newer posts than the post represented by its id
+        function getNewerPostList(id){
+            return getList('&newer='+id.toString());
+        }
+        
+        // // Get older posts than the post represented by its id
+        function getOlderPostList(id){
+            return getList('&older='+id.toString());
+        }
     }
  
