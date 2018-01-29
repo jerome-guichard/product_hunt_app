90c16dbde0a8c445be56ba943a4811a944e70f41
jerome-guichard <guichard_jerome@yahoo.fr>
2018-01-29 12:26:31 +0100

    Rename functions Add new tags in template

--- ProductHuntApp/app/core/graph/graph.directive.js
+++ ProductHuntApp/app/core/graph/graph.directive.js
@@ -5,7 +5,7 @@ angular.
     directive('stepChart', function($parse, $window){
     return{
        restrict:'EA',
-       template:"<svg width='850' height='400'></svg>",
+       template:"<p ng-if='!comments.postName'> Select a post in th list </p><p ng-if='comments.postName'> Current post : {{comments.postName}} </p><svg width='850' height='400'></svg>",
         link: function(scope, elem, attrs){
             var exp = $parse(attrs.chartData);          
             var commentCountToPlot=exp(scope);
@@ -14,13 +14,15 @@ angular.
             console.log(elem);
             console.log(attrs);
             console.log(exp);
+            
             var padding = 20;
             var pathClass="path";
             var xScale, yScale, xAxisGen, yAxisGen, lineFun;
 
             var d3 = $window.d3;
             var rawSvg=elem.find('svg');
-            var svg = d3.select(rawSvg[0]).attr("class", "graph-svg-component");;
+            var svg = d3.select(rawSvg[0])
+                    .attr("class", "graph-svg-component");;
 
             scope.$watchCollection(exp, function(newVal, oldVal){
                 
@@ -28,12 +30,16 @@ angular.
                 console.log(newVal);
 
                 commentCountToPlot=newVal;
+                
+                // If new set of comment data
                 if(typeof newVal !== 'undefined' ){
+                    // Print a step chart if no previous chart
                     if(typeof oldVal === 'undefined'){
-                        drawLineChart();
+                        drawStepChart();
                     }
+                    // if chart already creaated, replace data 
                     else{
-                        redrawLineChart();
+                        redrawStepChart();
                     }
                 }
          
@@ -41,27 +47,34 @@ angular.
 
             function setChartParameters(){
                 
+                // define limite on x-axis
+                // Set limit as last comment date + 1day
                 var endDate = new Date(commentCountToPlot[0].date);
                 endDate.setDate(commentCountToPlot[commentCountToPlot.length-1].date.getDate()+1);
-            
+                
+                // Scale x axis between the date of the first comment and last comment date + 1day
                 xScale = d3.time.scale()
                     .domain([commentCountToPlot[0].date, endDate])
                     .range([padding, rawSvg.attr("width") - padding * 2]);          
-
+                    
+                // Scale y axis between 0 and last count + 1
                 yScale = d3.scale.linear()
                     .domain([0, commentCountToPlot[commentCountToPlot.length-1].count + 1])
                     .range([rawSvg.attr("height") - padding, 0]);
-
+                
+                // x axis setting
                 xAxisGen = d3.svg.axis()
                     .scale(xScale)
                     .orient("bottom")
                     .ticks(10);
-
+            
+                // y axis setting
                 yAxisGen = d3.svg.axis()
                     .scale(yScale)
                     .orient("left")
                     .ticks(5);
-
+                
+                // define function as a step function
                 lineFun = d3.svg.line()
                     .x(function (d) {
                         return xScale(d.date);
@@ -72,20 +85,23 @@ angular.
                     
             }
 
-          function drawLineChart() {
+          function drawStepChart() {
 
                 setChartParameters();
-
+                
+                // append x axis to chart
                 svg.append("svg:g")
                     .attr("class", "x axis")
                     .attr('transform', 'translate(0,' + (rawSvg.attr("height")-padding) + ')')
                     .call(xAxisGen);
-             
+                
+                // append y axis to chart
                 svg.append("svg:g")
                     .attr("class", "y axis")
                     .attr("transform", "translate(20,0)")
                     .call(yAxisGen);
-
+            
+                // append x axis to step function
                 svg.append("svg:path")
                     .attr({
                         d: lineFun(commentCountToPlot),
@@ -94,28 +110,18 @@ angular.
                         "fill": "none",
                         "class": pathClass
                     });
-                    
+                
+                // append chart title
                 svg.append("text")
                     .attr("x", (rawSvg.attr("width") / 2))             
                     .attr("y", 15)
                     .attr("text-anchor", "middle")  
                     .style("font-size", "16px") 
-                    .style("text-decoration", "underline")  
-                    .text("Number of comments evolution over time");
-                // now add titles to the axes
-                svg.append("text")
-                    .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
-                    .attr("transform", "translate("+ (padding/2) +","+(height/2)+")rotate(-90)")  // text is drawn off the screen top left, move down and out and rotate
-                    .text("Value");
-
-                svg.append("text")
-                    .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
-                    .attr("transform", "translate("+ (width/2) +","+(height-(padding/3))+")")  // centre below axis
-                    .text("Date");
+                    .text("Comments count evolution over time");
             }
 
-            function redrawLineChart() {
-
+            function redrawStepChart() {
+                
                 setChartParameters();
 
                 svg.selectAll("g.y.axis").call(yAxisGen);
