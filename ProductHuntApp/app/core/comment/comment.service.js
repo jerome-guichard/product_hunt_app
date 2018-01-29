29cf5297aecdff965e5d0b111adcb8566a89b65e
jerome-guichard <guichard_jerome@yahoo.fr>
2018-01-29 12:27:30 +0100

    New function to tranform comments data provided by the API

--- ProductHuntApp/app/core/comment/comment.service.js
+++ ProductHuntApp/app/core/comment/comment.service.js
@@ -11,7 +11,9 @@ angular.
         
         var service = {
             // DATA
-            getList: getList,           
+            getList: getList,
+            filterCommentsByDate: filterCommentsByDate,
+            formatDataComments: formatDataComments
         };
 
         return service;
@@ -26,7 +28,9 @@ angular.
                 //Request string: sort posts by creation date in a descending order 
                 //Request string: limit the number of results
                 //Additional Request string: get post older/older than a given post 
-                url: url+'comments?search[post_id]='+postId+'&sort_by=created_at&order=asc',
+                //url: url+'comments?search[post_id]='+postId+'&sort_by=created_at&order=asc',
+                
+                url: url+'posts/'+postId+'/comments',
                 method: 'GET',
                 }).success(function(data,status,headers,config){
                     deferred.resolve(data);
@@ -35,6 +39,59 @@ angular.
                 });
 
             return deferred.promise;
+        }
+        
+        
+        function filterCommentsByDate(comments,limitDate){
+            
+            var chartStruct = [];
+            //Populate structure for the chart
+            for(var comIdx = 0; comIdx<comments.length; comIdx++){
+                
+                // date of the comments
+                var comDate = comments[comIdx];  
+                //if com date is beyond the limit break loop and return the struct
+                if(comDate < limitDate ){
+                    chartStruct.push({'date':comDate,'count':comIdx+1})
+                }else{
+                    break;
+                }
+            }
+            
+            return chartStruct;
+        }
+        
+        // Comments from API are nested, we need to transform into a plain object array
+        // Side-effect : sorting in data is broken 
+        function formatDataComments(comments){
+            
+            // Declare new array of comments
+            var formattedComments = [];
+            
+            // Iterate over comment array
+            for(var comIdx = 0; comIdx<comments.length; comIdx++)
+            {
+                // Extract only the date of the comment
+                formattedComments.push(new Date(comments[comIdx].created_at));
+                
+                // There are only 2 levels, so we can iterate directly without recursive call
+                if(comments[comIdx].child_comments_count > 0)
+                {   // Add child comments to comments array
+                    for(var childComIdx = 0; childComIdx<comments[comIdx].child_comments.length;childComIdx++ ){
+                        formattedComments.push(new Date(comments[comIdx].child_comments[childComIdx].created_at)); 
+                    }
+                }
+            }
+            
+            // sort date in asc order
+            var date_asc_sort  = function (date1, date2) {
+                if (date1 > date2) return 1;
+                if (date1 < date2) return -1;
+                return 0;
+            };
+            
+            // return sorted array
+            return formattedComments.sort(date_asc_sort);
         }
         
     }
